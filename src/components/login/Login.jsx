bc157cead686996448d5eedbe85d15660ce84c91
Anil <anil6996f@gmail.com>
2024-04-28 13:41:19 +0530

    Completed the Sign In functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './login.css';
 import { toast } from 'react-toastify';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../../lib/firebase';
 import { doc, setDoc } from 'firebase/firestore';
 import upload from '../../lib/upload';
@@ -51,8 +51,19 @@ const Login = () => {
         }
     }
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
+        setLoading(true);
+        const formData = new FormData(e.target);
+        const { email, password } = Object.fromEntries(formData);
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            toast.success("Logged In")
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
 
