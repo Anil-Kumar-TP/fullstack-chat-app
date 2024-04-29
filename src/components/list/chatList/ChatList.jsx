995092da4971dcb071a9eba7339273381999ea11
Anil <anil6996f@gmail.com>
2024-04-30 01:20:12 +0530

    Implemented the search functionality to search user by name

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -9,6 +9,7 @@ import { useChatStore } from '../../../lib/chatStore';
 const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
     const [chats, setChats] = useState([]);
+    const [input, setInput] = useState("");
 
     const { currentUser } = useUserStore();
     const { changeChat,chatId} = useChatStore();
@@ -49,16 +50,18 @@ const ChatList = () => {
         }
     }
 
+    const filteredChats = chats.filter((c) => c.user.username.toLowerCase().includes(input.toLowerCase()));
+
     return (
         <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
                     <img src="./search.png" alt="" />
-                    <input type="text" placeholder='Search...' />
+                    <input type="text" placeholder='Search...' onChange={(e)=>setInput(e.target.value)}/>
                 </div>
                 <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add' onClick={() => setAddMode((prev) => !prev)} />
             </div>
-            {chats?.map((chat) => (
+            {filteredChats?.map((chat) => (
             <div className="item" key={chat.chatId} onClick={()=>handleSelect(chat)} style={{backgroundColor: chat?.isSeen ? "transparent" : '#5183fe'}}>
                     <img src={chat.user.blocked.includes(currentUser.id) ? "./avatar.png" : chat.user.avatar || "./avatar.png"} alt="" />
                 <div className="texts">
