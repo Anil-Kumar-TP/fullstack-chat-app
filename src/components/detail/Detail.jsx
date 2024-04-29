249ba08ad010bcaa53c94cb7b86f815b27b38162
Anil <anil6996f@gmail.com>
2024-04-29 12:37:32 +0530

    Implemented SignOut functionality and fetch username and avatar

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './detail.css'
+import { auth } from '../../lib/firebase';
 
 const Detail = () => {
   return (
@@ -58,7 +59,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
-        <button className='logout'>LogOut</button>
+        <button className='logout' onClick={()=>auth.signOut()}>LogOut</button>
       </div>
     </div>
   )
